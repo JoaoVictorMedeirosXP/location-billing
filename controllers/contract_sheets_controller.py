7a87ab7b48ea3c771a124179e35af40ac73659e7
João Victor Medeiros <72466544+JoaoVictorMedeirosXP@users.noreply.github.com>
2025-06-06 17:56:48 -0300

    feat: router and controller

---
+++ controllers/contract_sheets_controller.py
@@ -0,0 +1,17 @@
+from flask import request, jsonify
+from factories.use_case_factory import make_process_contracts_use_case
+
+
+def process_contract_from_sheets_handler():
+    try:
+        body = request.get_json()
+        print("BODY:",body)
+        target_date = body.get("target_date", "2025-05-01")
+
+        use_case = make_process_contracts_use_case()
+        count = use_case.execute(target_date=target_date)
+
+        return jsonify({"status": "success", "written_rows": count}), 200
+
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)}), 500
---
+++ factories/use_case_factory.py
@@ -0,0 +1,34 @@
+from services.rental_contract_service import RentalContractService
+from services.contract_monitoring_service import ContractMonitoringService
+from services.summary_service import SummaryService
+from services.write_summay_service import SheetsWriterService
+
+from repositories.bigquery_repository import BigQueryRepository
+from repositories.firestore_repository import FirestoreRepository
+from repositories.sheets_repository import GoogleSheetsRepository
+
+from config.settings import SHEET_BILLS_NAME, SHEET_CONTRACTS_NAME
+from use_cases.process_contract_from_sheet_use_case import (
+    ProcessContractsFromSheetUseCase,
+)
+
+
+def make_process_contracts_use_case() -> ProcessContractsFromSheetUseCase:
+    rental_service = RentalContractService(
+        big_query_repo=BigQueryRepository(), firestore_repo=FirestoreRepository()
+    )
+
+    monitoring_service = ContractMonitoringService(
+        sheets_repo=GoogleSheetsRepository(SHEET_CONTRACTS_NAME)
+    )
+
+    writer_service = SheetsWriterService(
+        sheets_repo=GoogleSheetsRepository(SHEET_BILLS_NAME)
+    )
+
+    return ProcessContractsFromSheetUseCase(
+        rental_service=rental_service,
+        monitoring_service=monitoring_service,
+        writer_service=writer_service,
+        summary_service=SummaryService,
+    )
