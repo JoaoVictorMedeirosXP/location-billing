8b4b5082f3f4592c822f59b33c4b8e08aabbcf25
João Victor Medeiros <72466544+JoaoVictorMedeirosXP@users.noreply.github.com>
2025-06-25 17:25:01 -0300

    feat: summary bu message route and controller

---
+++ routes/summary_by_message_routes.py
@@ -0,0 +1,7 @@
+from flask import Blueprint
+from controllers.summary_by_message_controller import process_summary_by_message_handler
+
+summary_by_message_blueprint = Blueprint("summary-by-message", __name__)
+
+# POST /summary-by-message/
+summary_by_message_blueprint.route("/", methods=["POST"])(process_summary_by_message_handler)
